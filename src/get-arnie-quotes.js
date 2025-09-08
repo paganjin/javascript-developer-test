5c2215f6969a77cb83b02d5295d7fb9e0aecd2ee
Tianyou Jin <tianyoujin@Tianyous-MacBook-Pro.local>
2025-09-08 19:53:34 +1000

    implement simple solution for getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,19 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
+/**
+ * Fetches and processes quotes from multiple URLs in parallel
+ * @param {string[]} urls - Array of URLs to fetch quotes from
+ * @returns {Promise<Array<{ 'Arnie Quote': string } | { FAILURE: string }>>} - Array of result objects
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+	const responses = await Promise.all(urls.map((url) => httpGet(url)));
+
+	return responses.map(({ status, body }) => {
+		const { message } = JSON.parse(body);
+		return status === 200 ? { "Arnie Quote": message } : { FAILURE: message };
+	});
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
